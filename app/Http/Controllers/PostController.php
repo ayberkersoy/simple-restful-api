dbc7c197b4eaba8d7326182fbbc6e3062f3be09b
Ayberk Ersoy <ayberkersoy@gmail.com>
2017-01-25 00:29:58 +0300

    update posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
 use App\Post;
 use App\Topic;
 use App\Transformers\PostTransformer;
@@ -16,6 +17,20 @@ class PostController extends Controller
         $post->user()->associate($request->user());
 
         $topic->posts()->save($post);
+
+        return fractal()
+            ->item($post)
+            ->parseIncludes(['user'])
+            ->transformWith(new PostTransformer)
+            ->toArray();
+    }
+
+    public function update(UpdatePostRequest $request, Topic $topic, Post $post)
+    {
+        $this->authorize('update', $post);
+
+        $post->body = $request->get('body', $post->body);
+        $post->save();
 
         return fractal()
             ->item($post)
