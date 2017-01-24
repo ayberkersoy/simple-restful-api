fd6026c0d71febb1becde18b5cc4e269a3a3170e
Ayberk Ersoy <ayberkersoy@gmail.com>
2017-01-25 00:20:28 +0300

    reply topic with post

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePostRequest;
+use App\Post;
+use App\Topic;
+use App\Transformers\PostTransformer;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function store(StorePostRequest $request, Topic $topic, Post $post)
+    {
+        $post->body = $request->body;
+        $post->user()->associate($request->user());
+
+        $topic->posts()->save($post);
+
+        return fractal()
+            ->item($post)
+            ->parseIncludes(['user'])
+            ->transformWith(new PostTransformer)
+            ->toArray();
+    }
+}
