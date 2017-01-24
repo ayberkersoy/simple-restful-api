aab9215b1ec03318c164d678d1959d0eea85d349
Ayberk Ersoy <ayberkersoy@gmail.com>
2017-01-24 22:51:27 +0300

    update topic

--- routes/api.php
+++ routes/api.php
@@ -12,5 +12,6 @@ Route::group(['prefix' => 'topics'], function () {
     Route::get('/', 'TopicController@index');
     Route::get('{topic}', 'TopicController@show');
     Route::post('/', 'TopicController@store')->middleware('auth:api');
+    Route::patch('{topic}', 'TopicController@update')->middleware('auth:api');
 });
 
